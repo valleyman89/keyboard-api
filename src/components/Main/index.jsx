1ffdafd55933544670479daeecc5983534351f45
valleyman89 <dev@stevenrolph.com>
2023-08-09 12:48:52 +0100

    clean up

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,46 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Main.css";
 import PlayButton from "../PlayButton";
-import axios, { CanceledError } from "axios";
-import { useSettingsContext } from "../../context";
-import ErrorPage from "../ErrorPage";
 
 const Main = ({ data }) => {
-  // const { search } = useSettingsContext();
-  // const [data, setData] = useState();
-  // const [isLoading, setLoading] = useState(false);
-  // const [error, setError] = useState("");
-
-  // useEffect(() => {
-  //   const controller = new AbortController();
-  //   setLoading(true);
-  //   axios
-  //     .get("https://api.dictionaryapi.dev/api/v2/entries/en/" + search, {
-  //       signal: controller.signal,
-  //     })
-  //     .then((response) => {
-  //       setData(response.data[0]);
-  //     })
-  //     .catch((err) => {
-  //       if (err instanceof CanceledError) return;
-  //       setError(err.message);
-  //     })
-  //     .finally(() => {
-  //       setLoading(false); // do it old way if this doesn't work, strict mode on
-  //     });
-  //   console.log(data);
-  //   return () => controller.abort();
-  // }, [search]);
-
-  // if (search === null) return <ErrorPage />;
-
   return (
     <>
       <section>
         <div className="parent">
           <div className="div1">
             <h1>{data.word}</h1>
-            <mark>{data.phonetic}</mark>
+            {data.phonetic ? <mark>{data.phonetic}</mark> : null}
           </div>
           <div className="div2">
             <PlayButton />
